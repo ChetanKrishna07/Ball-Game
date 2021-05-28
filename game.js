c6f7f100f5cddd28967b5ba915b6fe956251638d
Chetan <chetanpalicherla@gmail>
2021-05-29 01:47:05 +0530

    Moving bug resolved, reset when start is clicked

--- game.js
+++ game.js
@@ -1,3 +1,39 @@
+var ballposs = {
+    row: 15,
+    col: 1,
+    running: 0,
+    level = 1
+}
+
+function createBoard() {
+    $(".board").html("");
+    var html = $(".board").html();
+    for(var row = 1; row < 16; row++) {
+        for(var col = 1; col < 16; col++) {
+            var cellClass = "cell " + "r" + row + "c" + col;
+             $(".board").html(html + "<div class='"+cellClass+"'></div>\n")
+             html = $(".board").html();
+             $(".r" + row + "c" + col).css("grid-row", row + "/" + (row+1));
+             $(".r" + row + "c" + col).css("grid-column", col + "/" + (col+1));
+        }
+    }
+    $(".r15c1").html("<img src='images/ball.png' alt = 'ball' class='ball' />");
+    ballposs.row = 15;
+    ballposs.col = 1;
+    ballposs.running = 1;
+    ballposs.level = 1;
+    $("heading").text("Level 1");
+}
+
+function move(frow, fcol, trow, tcol) {
+    fromClass = ".r" + frow + "c" + fcol;
+    toClass = ".r" + trow + "c" + tcol;
+    var html = $(fromClass).html();
+    console.log(fromClass)
+    $(fromClass).html("");
+    $(toClass).html(html); 
+}
+
 function pressAction(btn) {
     $(btn).addClass("pressed");
     setTimeout(function() {
@@ -7,34 +43,53 @@ function pressAction(btn) {
 
 $(".btn").click(function() { 
     pressAction(this);
+    createBoard();
 })
 
 $(document).keydown(function(e) {
     var k = e.key;
+    var row = ballposs.row;
+    var col = ballposs.col;
     if(k == " ") {
         pressAction(".btn");
-    } if(k == "ArrowRight") {
-        var x = $(".ball").css("grid-column");
-        x = parseInt(x[0]);
-
-        $(".ball").css("grid-column", x + 1);
-    }
-    if(k == "ArrowLeft") {
-        var x = $(".ball").css("grid-column");
-        x = parseInt(x[0]);
-
-        $(".ball").css("grid-column", x - 1);
-    }
-    if(k == "ArrowDown") {
-        var x = $(".ball").css("grid-row");
-        x = parseInt(x[0]);
-
-        $(".ball").css("grid-row", x + 1);
-    }
-    if(k == "ArrowUp") {
-        var x = $(".ball").css("grid-row");
-        x = parseInt(x[0]);
-
-        $(".ball").css("grid-row", x - 1);
+        createBoard();
+    } 
+    if(ballposs.running == 1) {
+        if(k == "ArrowRight") {
+            col = ((col) % 15) + 1;
+            console.log(col);
+            move(ballposs.row, ballposs.col, row, col);
+            ballposs.row = row;
+            ballposs.col = col;
+        }
+        if(k == "ArrowLeft") {
+            if(col > 1) {
+                col--;
+            } else {
+                col = 15;
+            }
+            console.log(col);
+            move(ballposs.row, ballposs.col, row, col);
+            ballposs.row = row;
+            ballposs.col = col;
+        }
+        if(k == "ArrowDown") {
+            row = ((row) % 15) + 1;
+            console.log(row);
+            move(ballposs.row, ballposs.col, row, col);
+            ballposs.row = row;
+            ballposs.col = col;
+        }
+        if(k == "ArrowUp") {
+            if(row > 1) {
+                row--;
+            } else {
+                row = 15;
+            }
+            console.log(row);
+            move(ballposs.row, ballposs.col, row, col);
+            ballposs.row = row;
+            ballposs.col = col;
+        }
     }
 })
